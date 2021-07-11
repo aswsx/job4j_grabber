94c215d1ebe3ba5c5e3f96f3e6692e4e3208a0c6
Tosh-PC <lextor78@gmail.com>
2021-07-11 22:31:57 +0600

    test

--- src/test/java/ru/job4j/TestTest.java
+++ src/test/java/ru/job4j/TestTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-class WhenIISTwo {
+public class TestTest {
     @Test
     public void whenIIsTwo() {
         var expected = 4;
