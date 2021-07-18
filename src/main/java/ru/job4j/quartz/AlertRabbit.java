b2a0ab992715646d187aece6b431414a17baa365
Tosh-PC <lextor78@gmail.com>
2021-07-18 14:14:49 +0600

    1. Quartz

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -1,34 +1,53 @@
 package ru.job4j.quartz;
 
-import org.quartz.*;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
 import org.quartz.impl.StdSchedulerFactory;
-import static org.quartz.JobBuilder.*;
-import static org.quartz.TriggerBuilder.*;
-import static org.quartz.SimpleScheduleBuilder.*;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
     public static void main(String[] args) {
         try {
             var scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(10)
+            var job = newJob(Rabbit.class).build();
+            var times = simpleSchedule()
+                    .withIntervalInSeconds(getInterval())
                     .repeatForever();
-            Trigger trigger = newTrigger()
+            var trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
 
+    private static int getInterval() {
+        var interval = 0;
+        var config = new Properties();
+        try {
+            var in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties");
+            config.load(in);
+            interval = Integer.parseInt(config.getProperty("rabbit.interval"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return interval;
+    }
+
+
     public static class Rabbit implements Job {
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
-            System.out.println("Rabbit runs here ...");
+        public void execute(JobExecutionContext context) {
+            System.out.println("The Truth Is Out There..." + getInterval());
         }
     }
 }
