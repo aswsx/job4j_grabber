e80db100b1e04c76f2bcd93c8cdbccd9a1e7d342
Tosh-PC <lextor78@gmail.com>
2021-07-18 19:49:53 +0600

    Tracker packages

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -1,10 +1,13 @@
 package ru.job4j.quartz;
 
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -13,18 +16,27 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
     public static void main(String[] args) {
+        var interval = getInterval();
         try {
+            List<Long> store = new ArrayList<>();
             var scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            var job = newJob(Rabbit.class).build();
+            var data = new JobDataMap();
+            data.put("store", store);
+            var job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             var times = simpleSchedule()
-                    .withIntervalInSeconds(getInterval())
+                    .withIntervalInSeconds(interval)
                     .repeatForever();
             var trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
+            Thread.sleep(5000);
+            scheduler.shutdown();
+            System.out.println(store);
         } catch (Exception se) {
             se.printStackTrace();
         }
@@ -44,9 +56,15 @@ public class AlertRabbit {
     }
 
     public static class Rabbit implements Job {
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) {
-            System.out.println("The Truth Is Out There..." + getInterval());
+            System.out.println("Rabbit runs here...");
+            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
+            store.add(System.currentTimeMillis());
         }
     }
 }
