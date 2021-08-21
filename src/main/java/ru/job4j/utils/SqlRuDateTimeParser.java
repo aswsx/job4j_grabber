6202e0a48c124a7707b709a647b135d7a2dc6b87
Tosh-PC <lextor78@gmail.com>
2021-08-22 01:49:13 +0600

    2.1. Преобразование даты

--- src/main/java/ru/job4j/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/utils/SqlRuDateTimeParser.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Map;
 
 import static java.util.Map.entry;
@@ -16,15 +17,15 @@ public class SqlRuDateTimeParser implements DateTimeParser {
      * Мапа хранит ключи- названия месяцев и соответствующую им нумерацию
      */
     private static final Map<String, String> MONTHS = Map.ofEntries(
-            entry("янв", "01"),
-            entry("фев", "02"),
-            entry("мар", "03"),
-            entry("апр", "04"),
-            entry("май", "05"),
-            entry("июн", "06"),
-            entry("июл", "07"),
-            entry("авг", "08"),
-            entry("сен", "09"),
+            entry("янв", "1"),
+            entry("фев", "2"),
+            entry("мар", "3"),
+            entry("апр", "4"),
+            entry("май", "5"),
+            entry("июн", "6"),
+            entry("июл", "7"),
+            entry("авг", "8"),
+            entry("сен", "9"),
             entry("окт", "10"),
             entry("ноя", "11"),
             entry("дек", "12")
@@ -44,23 +45,25 @@ public class SqlRuDateTimeParser implements DateTimeParser {
      */
     @Override
     public LocalDateTime parse(String parse) {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d M yy");
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-        String[] dateSplit = parse.split(",");
+        Locale locale = Locale.getDefault();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d M yy", locale);
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm", locale);
+        String[] dateSplit = parse.split(", ");
         String[] date = dateSplit[0].split(" ");
-        LocalDate localDate = LocalDate.now();
+        LocalDate localDate;
         LocalTime localTime = LocalTime.parse(dateSplit[1], timeFormatter);
 
         if (date.length == 3) { // если дата задана в виде "12 янв 2007"
-            LocalDate.parse(String.format("%s %s %s", date[0], MONTHS.get(date[1]), date[2]),
+            localDate = LocalDate.parse(String.format("%s %s %s", date[0], MONTHS.get(date[1]), date[2]),
                     dateFormatter);
         } else if (date[0].startsWith("сегодня")) { // если дата в виде "сегодня"
-            LocalDate.now();
+            localDate = LocalDate.now();
         } else if (date[0].startsWith("вчера")) { // если дата в виде "вчера"
-            LocalDate.now().minusDays(1);
+            localDate = LocalDate.now().minusDays(1);
         } else {
-            throw new IllegalArgumentException("Illegal date");
+            throw new IllegalArgumentException("Illegal date inputted");
         }
+
         return LocalDateTime.of(localDate, localTime);
     }
 }
