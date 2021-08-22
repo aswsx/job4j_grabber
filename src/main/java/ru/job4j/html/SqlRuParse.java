740d1dc0a54cb2e2fa5c391ec73d0171356b01f3
Tosh-PC <lextor78@gmail.com>
2021-08-22 14:05:11 +0600

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -2,12 +2,25 @@ package ru.job4j.html;
 
 import org.jsoup.Jsoup;
 
+import java.io.IOException;
+
 import static ru.exxo.jutil.Printer.println;
 
 public class SqlRuParse {
+    private static int index = 1;
+
     public static void main(String[] args) throws Exception {
-        var doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        String url = "https://www.sql.ru/forum/job-offers/";
+        for (int i = 1; i <= 5; i++) {
+            parser(url + i, index++);
+        }
+    }
+
+    private static void parser(String url, int index) throws IOException {
+        var i = 1;
+        var doc = Jsoup.connect(url).get();
         var row = doc.select(".postslisttopic");
+        println("******************** Страница " + index + " ********************");
         row.forEach(td -> {
             var href = td.child(0);
             println(href.attr("href"));
