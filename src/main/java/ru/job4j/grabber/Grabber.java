bafbf9192a3ffeb1ab8d69824138e3491ad788b4
Tosh-PC <lextor78@gmail.com>
2021-09-04 18:11:53 +0600

    7. Web

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -7,6 +7,10 @@ import ru.job4j.html.SqlRuParse;
 import ru.job4j.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -115,6 +119,28 @@ public class Grabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset
+                                    .forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
     public static void main(String[] args) throws Exception {
         var sqlDTParser = new SqlRuDateTimeParser();
         var grab = new Grabber();
@@ -122,5 +148,6 @@ public class Grabber implements Grab {
         var scheduler = grab.scheduler();
         var store = grab.store();
         grab.init(new SqlRuParse(sqlDTParser), store, scheduler);
+        grab.web(store);
     }
 }
