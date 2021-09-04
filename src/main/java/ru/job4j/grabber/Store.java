b2aca630c90fdb0db001dc6a6c3ab1510fd43df6
Tosh-PC <lextor78@gmail.com>
2021-09-04 15:49:25 +0600

    5. PsqlStore

--- src/main/java/ru/job4j/grabber/Store.java
+++ src/main/java/ru/job4j/grabber/Store.java
@@ -2,11 +2,10 @@ package ru.job4j.grabber;
 
 import ru.job4j.html.Post;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public interface Store {
-    void save(Post post) throws SQLException;
+    void save(Post post);
 
     List<Post> getAll();
 
