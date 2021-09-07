98a1d0835581e0f6d688753989737bc866021fb0
Tosh-PC <lextor78@gmail.com>
2021-09-07 21:43:20 +0600

    SqlRuParse refactoring

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,6 +4,7 @@ import org.jsoup.Jsoup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.job4j.grabber.Parse;
+import ru.job4j.utils.DateTimeParser;
 import ru.job4j.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
@@ -12,9 +13,9 @@ import java.util.List;
 
 public class SqlRuParse implements Parse {
     private static final Logger LOG = LoggerFactory.getLogger(SqlRuParse.class.getName());
-    private final SqlRuDateTimeParser dateTimeParser;
+    private final DateTimeParser dateTimeParser;
 
-    public SqlRuParse(SqlRuDateTimeParser dateTimeParser) {
+    public SqlRuParse(DateTimeParser dateTimeParser) {
         this.dateTimeParser = dateTimeParser;
     }
 
